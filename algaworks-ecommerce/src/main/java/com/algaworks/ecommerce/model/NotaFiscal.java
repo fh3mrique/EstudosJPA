d957c2847a30ef6a655be2a61b506aef54a10b8c
fh3mrique <97576330+fh3mrique@users.noreply.github.com>
2023-05-18 12:20:36 -0300

    Mapeando relacionamentos um para um com @JoinTable

--- algaworks-ecommerce/src/main/java/com/algaworks/ecommerce/model/NotaFiscal.java
+++ algaworks-ecommerce/src/main/java/com/algaworks/ecommerce/model/NotaFiscal.java
@@ -24,7 +24,10 @@ public class NotaFiscal {
 
     /*UMA NotaFiscal TEM UM Pedido e UM Pedido TEM UMA NotaFiscal*/
     @OneToOne
-    @JoinColumn(name = "pedido_id")
+    //@JoinColumn(name = "pedido_id")
+    @JoinTable(name = "pedido_nota_fiscal",
+    joinColumns = @JoinColumn(name = "nota_fiscal_id", unique = true),
+    inverseJoinColumns = @JoinColumn(name = "pedido_id", unique = true))
     private Pedido pedido;
 
     private String xml;
